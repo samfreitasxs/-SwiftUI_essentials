114ed81178133268823e8ba591dad18512f037ae
Samuel Freitas <37847692+samfreitasxs@users.noreply.github.com>
2024-11-21 09:26:01 -0300

    add DetailView

--- Scrumdinger/Models/DetailView.swift
+++ Scrumdinger/Models/DetailView.swift
@@ -10,17 +10,29 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
-
-
+    
+    
     var body: some View {
         List {
+            Section(header: Text("Meeting Info")) {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+            }
         }
     }
-}
-
-
-struct DetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailView(scrum: DailyScrum.sampleData[0])
+    
+    
+    struct DetailView_Previews: PreviewProvider {
+        static var previews: some View {
+            NavigationStack {
+                DetailView(scrum: DailyScrum.sampleData[0])
+            }
+        }
     }
 }
